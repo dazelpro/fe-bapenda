a660a0d17c616f9531165f3cd15ea4cc492f655b
adilsanak <adilsaputra0101@gmail.com>
2022-03-15 11:29:53 +0700

    Splash Screen

--- screens/Splash/Splash.js
+++ screens/Splash/Splash.js
@@ -1,6 +1,14 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image } from 'react-native'
 import React, { Component } from 'react'
 import { StackActions } from '@react-navigation/native';
+import {
+    COLORS,
+    FONTS,
+    SIZES,
+    constants,
+    icons,
+    dummyData
+} from '../../constants';
 
 class Splash extends Component {
     constructor(props){
@@ -11,13 +19,48 @@ class Splash extends Component {
     componentDidMount = () => {
         setTimeout(() => {
             this.props.navigation.dispatch(StackActions.replace('Home'))
-        }, 3000)
+        }, 2000)
     };
 
     render() {
         return (
-            <View>
-                <Text>Splash</Text>
+            <View
+                style={{ 
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: COLORS.primary
+                }}
+            >
+                <Image
+                    source={dummyData?.appProfile?.profile_image}
+                    style={{ 
+                        width: 100,
+                        height: 100
+                    }}
+                >
+
+                </Image>
+
+                <Text
+                    style={{ 
+                        marginTop: SIZES.padding,
+                        color: COLORS.white,
+                        ...  FONTS.h3
+                    }}
+                >
+                    BADAN PENDAPATAN DAERAH
+                </Text>
+
+                <Text
+                    style={{ 
+                        marginTop: SIZES.base,
+                        color: COLORS.white,
+                        ...  FONTS.h3
+                    }}
+                >
+                    KOTA PADANG
+                </Text>
             </View>
         )
     }
