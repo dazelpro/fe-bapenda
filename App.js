20a8ff6676d010dd65b5def812a532a29baa6022
adilsanak <adilsaputra0101@gmail.com>
2022-03-18 00:07:35 +0700

    Home

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const App = () => {
                     screenOptions={{
                         headerShown: false
                     }}
-                    initialRouteName={'Splash'}
+                    initialRouteName={'DetailArticle'}
                 >
                     <Stack.Screen
                         name="Home"
